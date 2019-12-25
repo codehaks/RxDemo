f8802aba1c1af77eec88b1ac1fb49ebab0860ade
Hakim Ghods <ghods.hakim@hotmail.com>
2019-12-26 02:24:46 +0330

    Non-blocking

--- RxDemo/Program.cs
+++ RxDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace RxDemo
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
@@ -20,27 +21,54 @@ namespace RxDemo
 
             //}
 
-            var magic = new AsyncMagicalNumberGenerator();
-            await foreach (var item in magic.Generate(5))
-            {
-                Console.WriteLine($"{item}");
+            //var magic = new AsyncMagicalNumberGenerator();
+            //await foreach (var item in magic.Generate(5))
+            //{
+            //    Console.WriteLine($"{item}");
 
-            }
+            //}
 
+            var magic = new MagicalNumberGenerator();
+            var subscription = magic.GenerateNumber(5)
+                .Timestamp()
+                .Subscribe((number) => Console.WriteLine($"{number.Value} - {number.Timestamp.Second}"));
+
+            Console.ReadLine();
 
         }
     }
 
     class MagicalNumberGenerator
     {
-        public IEnumerable<int> Generate(int amount) {
+        public IEnumerable<int> Generate(int amount)
+        {
             for (int i = 0; i < amount; i++)
             {
                 Thread.Sleep(2000);
                 yield return new Random().Next(10, 100);
             }
         }
+
+        public IObservable<int> GenerateNumber(int amount)
+        {
+            return Observable.Create<int>((o,ct) =>
+            {
+                return Task.Run(() =>
+                {
+                    foreach (var prime in Generate(amount))
+                    {
+                        ct.ThrowIfCancellationRequested();
+                        o.OnNext(prime);
+
+                    }
+                    o.OnCompleted();
+                });
+                //return Disposable.Empty;
+            });
+        }
     }
+
+
 
     class AsyncMagicalNumberGenerator
     {
