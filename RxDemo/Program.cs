f50a2f1e9945dbf76602490731cbd8a871002d1b
Hakim Ghods <ghods.hakim@hotmail.com>
2019-12-26 01:41:25 +0330

    Add project files.

---
+++ RxDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace RxDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var numbers = Enumerable.Range(1, 10);
+            var observableQuery = numbers.ToObservable();
+
+            observableQuery.Subscribe((n) =>
+            {
+                Console.WriteLine($" Number = {n} ");
+            },
+            _ =>
+            {
+
+                Console.WriteLine("Done!");
+            });
+
+
+        }
+    }
+}
