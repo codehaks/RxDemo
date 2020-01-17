92f77b6137ac988f582f2a38d156dfd6f698dbb2
Hakim Ghods <ghods.hakim@hotmail.com>
2020-01-17 03:45:57 +0330

    Rx

---
+++ RxWinForm/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RxWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var movingEvents = Observable
+                .FromEventPattern<MouseEventHandler, MouseEventArgs>
+                (   h => this.MouseMove += h,
+                    h => this.MouseMove -= h);
+
+            var mouseMove = from e1 in movingEvents
+                             select e1;
+
+            mouseMove.Subscribe((d) => {
+
+                textBox1.Text = d.EventArgs.X.ToString();
+                textBox2.Text = d.EventArgs.Y.ToString();
+            });
+
+           
+        }
+
+    }
+}
